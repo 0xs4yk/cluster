5b2a0fc812bc0e9b218c26d7f3d2cb82b0c379d8
saykdoteth <lancosme.maxence@gmail.com>
2024-02-06 22:23:19 +0100

    pv,pvc,app go

---
+++ app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "interface.html")
+	})
+
+	port := 8080
+	fmt.Printf("Serveur en cours d'exécution sur le port %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
